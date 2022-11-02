10b403d66b4b49bfd084628aeb14469e19a6d039
jennybigman <jennifersbigman@gmail.com>
2022-11-02 16:22:42 -0700

    kelly's code to download and concatenate hindcast data from ROMS server

---
+++ download and concatenate hindcast variables from pmel server.R
@@ -0,0 +1,60 @@
+library(ncdf4)
+library(lubridate)
+library(abind)
+
+#-------------------
+# Setup
+#-------------------
+
+yrblock <- seq(1985, 2010, by=5) # Calibration block ####### change this to 1970, 2020!!!!!
+
+# Variable to read 
+
+vname1 <- "temp_bottom5m" # Variable name in filename
+vname2 <- "temp"          # Variable name in file
+
+# Simulation names
+
+hcsim <- "B10K-K20_CORECFS"                  # hindcast
+
+# The naming conventions for files are slightly different on mox compared to on the 
+# PMEL server (PMEL server adds an extra folder level, with 5-year blocks).  In this script,
+# we're reading hindcast data from the PMEL server (because it's faster than over the mox
+# ssh connection) and forecast data from mox b/c it hasn't been added to the server yet.
+
+tdsbase <- "https://data.pmel.noaa.gov/aclim/thredds/dodsC" # top-level folder, PMEL server
+
+	#--------------------
+	# Read hindcast data
+	#--------------------
+	
+	for (yr in yrblock) {
+
+  fname <- file.path(tdsbase, 
+                     hcsim, 
+                     "Level2",
+                     paste0(yr, "-", yr+4), 
+                     paste0(hcsim, "_", yr, "-", yr+4, "_average_", vname1, ".nc"))
+  
+  # Read data
+  
+  ncin <- nc_open(fname)
+  
+  # Read data
+  
+  ttmp <- ncvar_get(ncin,"ocean_time")
+  ttmp <- lubridate::ymd("1900-01-01") + lubridate::seconds(ttmp)
+  vtmp <- ncvar_get(ncin, vname2)
+  
+  nc_close(ncin)
+  
+  # Concatenate across 5-year blocks
+  
+  if (yr == yrblock[1]){
+    thc <- ttmp
+    vhc <- vtmp
+  } else {
+    thc <- c(thc, ttmp)
+    vhc <- abind(vhc, vtmp)
+  }
+}
